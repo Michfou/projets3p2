b8bb4a58ebd23320c25b6429e239039a607eb41f
Hugo Bedard <hugo_bedard@hotmail.com>
2013-09-23 19:41:25 -0400

    Added effect to page changes when clicking on left icons

--- UserInterface/js/script.js
+++ UserInterface/js/script.js
@@ -13,4 +13,22 @@ $(function() {
 
 	$(".left-nav-link").hover(leftNavHoverHandlerIn, leftNavHoverHandlerOut);
 
+	$(".left-nav-link").click(function() {
+
+		/*var value = $(".content-container").css("left");
+		$(".content-container").css("left", "170px");
+
+		for (var i = 0; i < 100; i += 10) {
+			value = parseInt(value) + parseInt(i);
+			$(".content-container").css("right", "-" + value + "px");
+			$(".content-container").css("left", value + "px");
+		};*/
+
+		$(".alarm-system-content").css("display","none");//effect("fade", {mode : "hide"}, 500);
+		
+		$(".camera-content").effect("slide", {direction: "left"}, 500);
+		$(".camera-content").css("display", "inline-block");
+
+	});
+
 });
